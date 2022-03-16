beb5b918b70e7202f8759ba789ea6b6ec5e73b0a
fathan <fathan.a.dev@gmail.com>
2022-03-16 10:29:22 +0700

    fix get pelajaran INNER JOIN

--- app/models/pelajaran_db.js
+++ app/models/pelajaran_db.js
@@ -13,7 +13,7 @@ module.exports = {
     getPelajaranDB: async (data)=>{
         const conn = client()
         await conn.connect()
-        const res = await conn.query("SELECT * FROM pelajaran WHERE kelas = '"+data+"'")
+        const res = await conn.query("SELECT pelajaran.*, users.nama AS nama_guru FROM pelajaran INNER JOIN users ON pelajaran.guru_id = users.id WHERE pelajaran.kelas = '"+data+"'")
         await conn.end()
         return res
     }
--- index.js
+++ index.js
@@ -23,7 +23,7 @@ initAuthRoutes(app)
 
 initSocketConnection(io)
 
-server.listen(3000, () => {
+server.listen(port, () => {
   console.log(`listening at http://localhost:${port}`)
 })
 
