f3c9a5092eae43f6010eef23f35a673c096275b4
fathan <fathan.a.dev@gmail.com>
2022-03-24 10:47:34 +0700

    attendance

--- app/models/attendance_db.js
+++ app/models/attendance_db.js
@@ -23,7 +23,7 @@ module.exports = {
     getAttendanceByPelajaranDB: async (data)=>{
         const conn = client()
         await conn.connect()
-        const select = "hadir.*, pelajaran.*, users.nama AS nama_siswa"
+        const select = "hadir.*, users.nama AS nama_siswa"
         const res = await conn.query("SELECT "+select+" FROM hadir INNER JOIN pelajaran ON pelajaran.guru_id = hadir.guru_id INNER JOIN users ON users.id = hadir.siswa_id WHERE pelajaran.id = "+data)
         await conn.end()
         return res
