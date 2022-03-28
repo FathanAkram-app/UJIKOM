b740cd5ee314cea691b7942ed08adbae4be706f9
fathan <fathan.a.dev@gmail.com>
2022-03-28 20:16:48 +0700

    pelajaran by kelas fix

--- app/models/pelajaran_db.js
+++ app/models/pelajaran_db.js
@@ -7,20 +7,9 @@ module.exports = {
     getPelajaranByKelasDB: async (data)=>{
         const conn = client()
         await conn.connect()
-        const usersArray = "ARRAY(SELECT id FROM users WHERE kelas = '"+data+"') AS id_siswa, ARRAY(SELECT nama FROM users WHERE kelas = '"+data+"') AS nama_siswa"
-        const res = await conn.query("SELECT pelajaran.*, users.nama AS nama_guru, "+usersArray+" FROM pelajaran INNER JOIN users ON pelajaran.guru_id = users.id WHERE pelajaran.kelas = '"+data+"'")
-        for (const key in res.rows) {
-            const a = res.rows[key].id_siswa
-            
-            const arr = []
-            for (const i in a) {
-                arr.push({id_siswa: res.rows[key].id_siswa[i], nama_siswa: res.rows[key].nama_siswa[i]})
-            }
-            res.rows[key].siswa = arr
-            delete res.rows[key].id_siswa
-            delete res.rows[key].nama_siswa
-            
-        }
+        const res = await conn.query("SELECT pelajaran.*, users.nama AS nama_guru, hadir.status FROM pelajaran INNER JOIN users ON pelajaran.guru_id = users.id LEFT JOIN hadir ON hadir.pelajaran_id = pelajaran.id WHERE pelajaran.kelas = '"+data+"' ")
+
+
         await conn.end()
         return res
     },
